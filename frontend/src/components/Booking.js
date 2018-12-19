34e606bac4672bb706167c923085f50783cc16d2
Julie Gicquel <julie.gcql@gmail.com>
2018-12-19 15:50:16 +0100

    create booking page

---
+++ frontend/src/components/Booking.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Booking extends Component {
+  render() {
+    return (
+      <div>
+        <button>
+
+          <img>
+
+        </button>
+        
+      </div>
+    )
+  }
+}
