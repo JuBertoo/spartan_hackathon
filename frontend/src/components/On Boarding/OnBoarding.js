818a29e4a13aa27393c5a74ad112b01456d6919f
Julie Gicquel <julie.gcql@gmail.com>
2018-12-20 11:45:32 +0100

    create onboarding component

---
+++ frontend/src/components/On Boarding/OnBoarding.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import './OnBoarding.scss'
+
+export default class OnBoarding extends Component {
+  render() {
+    return (
+      <div>
+          <div className="onBoardingTitle">
+            <h1>ONBOARDING</h1>
+					</div>
+					<div className="onBoardingLogo">
+						<img src="https://via.placeholder.com/200"/>				
+					</div>
+					<div className="onBoardingDescription">
+						<p>Donne la possibilité à tous ceux qui ont besoin de laver ou sécher du linge hors domicile de minimiser les temps d'attente, de fluidifier les étapes et de sécuriser son linge en se connectant aux équipements</p>
+					</div>
+					<div className="onBoardingPictos">
+						<div className="FirstPicto">
+							<img src="https://via.placeholder.com/80"/>
+							<p>Lavage</p>				
+						</div>
+						<div className="SecondPicto">
+							<img src="https://via.placeholder.com/80"/>
+							<p>Séchage</p>				
+						</div>
+						<div className="ThirdPicto">
+							<img src="https://via.placeholder.com/80"/>
+							<p>Drop & Pick up</p>				
+						</div>
+					</div>
+					<div className="onBoardingFleche">
+						<img src={require('./arrow.png')}/>
+					</div>
+        
+      </div>
+    )
+  }
+}
--- frontend/src/routes/PublicRouter.js
+++ frontend/src/routes/PublicRouter.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Profile from '../components/Profile';
 import { connect } from 'react-redux';
+import OnBoarding from '../components/On Boarding/OnBoarding';
 
 class AppRouter extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Profile}/>
+        <Route path="/onboarding" component={OnBoarding}/>
       </Switch>
     )
   }
