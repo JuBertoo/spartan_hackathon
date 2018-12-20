42c15a8c694831864d7f1b4d0f378ee29de84331
Julien <julien.bertocchini@gmail.com>
2018-12-20 15:05:32 +0100

    Create a map component

--- frontend/src/components/Map/LaverieMap.js
+++ frontend/src/components/Map/LaverieMap.js
@@ -1,61 +1,114 @@
-// import React, { Component, Fragment } from 'react'
-// import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-
-
-// export default class LaverieMap extends Component {
-//   constructor() {
-//     super()
-//     this.state = {
-//       lat: 51.505,
-//       lng: -0.09,
-//       zoom: 13
-//     }
-//   }
-
-//   render() {
-//     const position = [this.state.lat, this.state.lng];
-//     return (
-//       <Map center={position} zoom={this.state.zoom}>
-//         <TileLayer
-//           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-//           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-//         />
-//         <Marker position={position}>
-//           <Popup>
-//             A pretty CSS3 popup. <br/> Easily customizable.
-//           </Popup>
-//         </Marker>
-//       </Map>
-//     );
-//   }
-// }
-
 import React, { Component } from 'react'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import laverieDatas from './laverieDatas.json'
+import { renderToStaticMarkup } from "react-dom/server";
+import { divIcon } from "leaflet";
+import './LaverieMap.scss'
 
-//
+const setIcon = (integer, color) => {
+  return (
+    divIcon({
+      html: renderToStaticMarkup(
+        <i style={{color: color}} className="fa fa-map-marker-alt fa-3x">{integer}</i>
+      )
+    })
+  )
+}
 
-export default class SimpleExample extends Component{
+
+export default class LaverieMap extends Component{
   state = {
-    lat: 51.505,
-    lng: -0.09,
-    zoom: 13,
+    initialMap: {
+      lat: 43.3008396, 
+      lng: 5.367089399999941, 
+      zoom: 15,
+    },
+    iconColor: "grey",
+    currentFilter: 'nbMachine',
+    currentSelected: {},
+    visible: false,
+    level:null
+    
+
+  }
+
+  // componentDidMount(){
+  //   this.state.iconColor;
+  // }
+
+  getMachine=()=>{
+    // if(this.state.currentFilter.level > 0){
+    this.setState({iconColor: "red", currentFilter: 'nbMachine'}) 
+    // }
+  }
+  getSechoir=()=>{
+    // if(this.state.currentFilter.level > 0){
+    this.setState({iconColor: "green", currentFilter: 'nbSechoir'}) 
+    // }
+  }
+  getLocker=()=>{
+    // if(this.state.currentFilter.level > 0){
+    this.setState({iconColor: "yellow", currentFilter: 'nbCasier'}) 
+    // }
   }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    console.log(this.state);
+    const position = [this.state.initialMap.lat, this.state.initialMap.lng];
+    let visibility = this.state.visible ? "visible" : "hidden";
+
     return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </Map>
+    
+      
+      <div className="laverieMapContainer">
+        <div className="mapContainer">
+       
+          <Map center={position} zoom={this.state.initialMap.zoom} onClick={() => this.setState({visible: false})}>
+          <div className="buttonType">
+            <button onClick={()=>this.getMachine()}> machine </button>
+            <button onClick={()=>this.getSechoir()}> Seche-linge </button>
+            <button onClick={()=>this.getLocker()}> Locker </button>
+          </div>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+            laverieDatas.map((laverie) => {
+              const icon = laverie.level > 0 ? setIcon( `${laverie[this.state.currentFilter]}`, `${this.state.iconColor}` ) : setIcon( `${laverie[this.state.currentFilter]}`, 'grey' )
+              return(
+
+                <Marker position={laverie.position} icon={icon}  
+                onClick={() => this.setState({currentSelected: laverie , visible: !this.state.visible})}>
+                  
+                </Marker>
+                  )
+                })
+              }
+          </Map>
+        </div>
+      
+        <div className={visibility}>
+              <div className="adresseContainer">
+                <img src={this.state.currentSelected.image}/>
+                <div className="adresse">
+                  <h3>{this.state.currentSelected.title}</h3>
+                  <p>{this.state.currentSelected.adresse}</p>
+                  <p>{this.state.currentSelected.horraire}</p>
+
+                </div>
+                <div className="adresseImage">
+
+                </div>
+              </div>
+              <div className="modeImage">
+                <button>Laver</button>
+                <button>Secher</button>
+                <button>Deposer</button>
+              </div>
+        </div>
+      </div>    
+
     )
   }
 }
