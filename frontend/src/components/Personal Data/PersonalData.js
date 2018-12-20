ce14c2ad4b8313299109166a0e80557c43d9da0f
Julie Gicquel <julie.gcql@gmail.com>
2018-12-20 10:48:34 +0100

    create credit card & complete form

--- frontend/src/components/Personal Data/PersonalData.js
+++ frontend/src/components/Personal Data/PersonalData.js
@@ -1,119 +1,242 @@
 import React, { Component } from 'react'
+import 'react-credit-cards/es/styles-compiled.css';
+import Card from 'react-credit-cards';
+import {
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate,
+  formatFormData,
+} from './utils';
+import './PersonalData.scss';
 
 export default class PersonalData extends Component {
   state = {
-    firstNameCompleted : 'input-text',
-    emailCompleted : 'input-text',
-    phoneCompleted : 'input-text',
-    messageCompleted : 'input-text',
     firstName : '',
     email : '',
     phone : '',
+    password : '',
+    number: '',
+    name: '',
+    expiry: '',
+    cvc: '',
+    issuer: '',
+    focused: '',
+    formData: null,
   }
 
+  handleChange = (e) => {
+    console.log(e.target.value);
+    const currentFieldName = e.target.name
+
+    this.setState({[currentFieldName]: e.target.value})
+    
+    if(e.target.value !== ''){
+      this.setState({[`${currentFieldName}Completed`] : 'input-text not-empty'})
+    } else {
+      this.setState({[`${currentFieldName}Completed`] : 'input-text'})
+    }
+  } 
+
+  handleCallback = ({ issuer }, isValid) => {
+    if (isValid) {
+      this.setState({ issuer });
+    }
+  };
+
+  handleInputFocus = ({ target }) => {
+    this.setState({
+      focused: target.name,
+    });
+  };
+
+  handleInputChange = ({ target }) => {
+    if (target.name === 'number') {
+      target.value = formatCreditCardNumber(target.value);
+    } else if (target.name === 'expiry') {
+      target.value = formatExpirationDate(target.value);
+    } else if (target.name === 'cvc') {
+      target.value = formatCVC(target.value);
+    }
+
+    this.setState({ [target.name]: target.value });
+  };
 
   render() {
+    const { name, number, expiry, cvc, focused, issuer, formData } = this.state;
     return (
       <div>
-      <div className="dataTitle">
-        <h1>INFOS PERSONNELLES</h1>
+        <div className="dataTitle">
+          <h1>INFOS PERSONNELLES</h1>
+        </div>
+        <div className="ContactContainer">
+
+          <form 
+          className="contact-form"
+          onSubmit={(e) => this.handleSubmit(e)}>
+
+          {/* NAME FIELD */}
+
+            <div className="form-fields">
+              <label 
+                className="label" 
+                htmlFor="firstName">
+              Nom *
+              </label>
+              <input 
+                type="text" 
+                name="firstName" 
+                id="firstName" 
+                value={this.state.firstName}
+                onChange={(e) => this.handleChange(e)} 
+                required>
+              </input>
+
+              
+            </div>
+            
+          {/* EMAIL FIELD */}
+
+            <div className="form-fields">
+              <label 
+              className="label" 
+              htmlFor="email">
+              Email *
+              </label>
+              <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                value={this.state.email}
+                onChange={(e) => this.handleChange(e)} 
+                required>
+              </input>
+              
+            </div>
+
+            {/* PASSWORD FIELD */}
+
+            <div className="form-fields">
+              <label 
+              className="label" 
+              htmlFor="password">
+              Password *
+              </label>
+              <input 
+                type="password" 
+                name="password" 
+                id="password" 
+                value={this.state.password}
+                onChange={(e) => this.handleChange(e)} 
+                required>
+              </input>            
+            </div>
+
+          {/* PHONE FIELD */}
+
+
+            <div className="form-fields">
+              <label 
+                className="label" 
+                htmlFor="phone">
+              Téléphone *
+              </label>
+              <input 
+                type="tel" 
+                name="phone" 
+                id="phone" 
+                value={this.state.phone}
+                onChange={(e) => this.handleChange(e)} 
+                required>
+              </input>
+              
+            </div>
+
+          {/* BUTTON */}
+
+            <div className="form-fields button">
+              <button 
+              className="btn btn-warning"
+              >Envoyer</button>   
+            </div>
+
+          </form>
+        
+    {/* CREDIT CARD */}
+          <div key="Payment">
+            <div className="App-payment">
+              <Card
+                number={number}
+                name={name}
+                expiry={expiry}
+                cvc={cvc}
+                focused={focused}
+                callback={this.handleCallback}
+              />
+              <form ref={c => (this.form = c)} onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <input
+                    type="tel"
+                    name="number"
+                    className="form-control"
+                    placeholder="Card Number"
+                    pattern="[\d| ]{16,22}"
+                    required
+                    onChange={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Name"
+                    required
+                    onChange={this.handleInputChange}
+                    onFocus={this.handleInputFocus}
+                  />
+                </div>
+                <div className="row">
+                  <div className="col-6">
+                    <div className="form-group">
+                      <input
+                        type="tel"
+                        name="expiry"
+                        className="form-control"
+                        placeholder="Valid Thru"
+                        pattern="\d\d/\d\d"
+                        required
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="form-group">
+                      <input
+                        type="tel"
+                        name="cvc"
+                        className="form-control"
+                        placeholder="CVC"
+                        pattern="\d{3,4}"
+                        required
+                        onChange={this.handleInputChange}
+                        onFocus={this.handleInputFocus}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <input type="hidden" name="issuer" value={issuer} />
+                <div className="form-fields button">
+                  <button 
+                  className="btn btn-warning"
+                  >Payer</button>   
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="ContactContainer">
-
-        <form 
-        className="contact-form"
-        onSubmit={(e) => this.handleSubmit(e)}>
-
-        {/* NAME FIELD */}
-
-          <div className="form-fields">
-            <input 
-              type="text" 
-              name="firstName" 
-              id="firstName" 
-              value={this.state.firstName}
-              onChange={(e) => this.handleChange(e)} 
-              className={this.state.firstNameCompleted} 
-              required>
-            </input>
-
-            <label 
-              className="label" 
-              htmlFor="firstName">
-            Nom *
-            </label>
-          </div>
-          
-        {/* EMAIL FIELD */}
-
-          <div className="form-fields">
-            <input 
-              type="email" 
-              name="email" 
-              id="email" 
-              value={this.state.email}
-              onChange={(e) => this.handleChange(e)} 
-              className={this.state.emailCompleted} 
-              required>
-            </input>
-            <label 
-            className="label" 
-            htmlFor="email">
-            Email *
-            </label>
-          </div>
-
-          {/* PASSWORD FIELD */}
-
-          <div className="form-fields">
-            <input 
-              type="password" 
-              name="password" 
-              id="password" 
-              value={this.state.password}
-              onChange={(e) => this.handleChange(e)} 
-              className={this.state.passwordCompleted} 
-              required>
-            </input>
-            <label 
-            className="label" 
-            htmlFor="password">
-            Password *
-            </label>
-          </div>
-
-        {/* PHONE FIELD */}
-
-
-          <div className="form-fields">
-            <input 
-              type="tel" 
-              name="phone" 
-              id="phone" 
-              value={this.state.phone}
-              onChange={(e) => this.handleChange(e)} 
-              className={this.state.phoneCompleted} 
-              required>
-            </input>
-            <label 
-              className="label" 
-              htmlFor="phone">
-            Téléphone *
-            </label>
-          </div>
-
-        {/* BUTTON */}
-
-          <div className="form-fields button">
-            <button 
-            className="btn btn-warning"
-            >Envoyer</button>   
-          </div>
-
-        </form>
-      
-    </div>
-    </div>
 
     )
   }
