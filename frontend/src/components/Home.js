f8844a8a30a513e7e9820ce08b18829010f367aa
Julie Gicquel <julie.gcql@gmail.com>
2018-12-20 18:47:40 +0100

    add images in home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import './Home.scss'
 
 export default class Home extends Component {
   render() {
+    console.log('HOME')
     return (
-      <div>
+      <div className="homePage">
+        <div className="logoHome">
+          <img src={require('./WiHub-05.png')}/>
+        </div>
+        <div className="logoText">
+          <img src={require('./WiHub-06.png')}/>
+        </div>
       </div>
     )
   }
