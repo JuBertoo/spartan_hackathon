2bf3b889e46634268843e4ccc0cd0f6c83af0e3b
Julie Gicquel <julie.gcql@gmail.com>
2018-12-20 15:07:55 +0100

    add calendar picker

--- frontend/src/routes/PublicRouter.js
+++ frontend/src/routes/PublicRouter.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import {Switch, Route, withRouter} from 'react-router-dom';
 import Profile from '../components/Profile';
 import { connect } from 'react-redux';
+import Booking from '../components/Booking/Booking'
 
 class AppRouter extends Component {
   render() {
     return (
       <Switch>
         <Route exact path="/" component={Profile}/>
+        <Route path="/booking" component={Booking}/>
       </Switch>
     )
   }
