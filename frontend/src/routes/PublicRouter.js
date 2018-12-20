c367bff8f9046b0251dcc05657af1958c876058d
Julien <julien.bertocchini@gmail.com>
2018-12-20 16:21:02 +0100

    resolv conflict

--- frontend/src/routes/PublicRouter.js
+++ frontend/src/routes/PublicRouter.js
@@ -8,26 +8,22 @@ import myBookings from '../components/My Bookings/myBookings';
 import PersonalData from '../components/Personal Data/PersonalData';
 
 import OnBoarding from '../components/On Boarding/OnBoarding';
+import LaverieMap from '../components/Map/LaverieMap';
 
 
 class PublicRouter extends Component {
   render() {
     return (
       <Switch>
+        
         <Route path="/booking" component={Booking}/>
-
-     
-        <Route path="/booking" component={Booking}/>
-
-
         <Route path="/personaldata" component={PersonalData}/>
-
         <Route path="/onboarding" component={OnBoarding}/>
         <Route exact path="/" component={Home}/>
-
-
         <Route path="/reservations" component={myBookings}/>
+        <Route path="/map" component={LaverieMap}/>
       </Switch>
+        
     )
   }
 }
